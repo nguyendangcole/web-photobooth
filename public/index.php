dc52be84ecef5475ca40502991e4544bf6489210
nguyencolece <nguyencolece@192.168.2.9>
2025-09-23 22:39:20 +0700

    First commit - initial project structure

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Photobooth</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <h1>Welcome to Photobooth</h1>
+  <p>Website chạy trên MAMP localhost.</p>
+  <script src="js/app.js"></script>
+</body>
+</html>
